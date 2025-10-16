b4952e3fbb0c651bde66bbd874e26c1bb7e47f75
NISHA NAJIHAH <nishanajihah88@gmail.com>
2025-10-16 23:01:04 +0800

    feat: AdMob environment setup and native config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        
+        resValue "string", "admob_app_id", ADMOB_APP_ID
     }
 
     buildTypes {
